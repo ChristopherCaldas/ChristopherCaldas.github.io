4411948229261a19c99099ffd3cead4673653039
ChristopherCaldas <christophercaldas16@gmail.com>
2020-09-12 23:25:23 +0100

    Formatting Changes
    Changed Column/Row layouy
    Fixed typos

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ document.getElementById("menu1").addEventListener("click", openMenu1);
 document.getElementById("menu2").addEventListener("click", openMenu2);
 function openMenu1() {
 	document.getElementById("dropdown1").classList.toggle("active");
-
 }
 function openMenu2() {
 	document.getElementById("dropdown2").classList.toggle("active");
-;
 }
