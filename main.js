3a19cc359faad0ecbbfabeb0b7308a44d479c23b
ChristopherCaldas <51334879+ChristopherCaldas@users.noreply.github.com>
2023-01-03 13:37:41 -0500

    Setting stage for creation of MMSA Tool

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ function openMenu1() {
 function openMenu2() {
 	document.getElementById("dropdown2").classList.toggle("active");
 }
+
+function findSum() {
+  var num1 = document.getElementById("num1").value;
+  var num2 = document.getElementById("num2").value;
+  var num3 = document.getElementById("num3").value;
+  var sum = parseInt(num1) + parseInt(num2) + parseInt(num3);
+  alert("The sum is: " + sum);
+}
