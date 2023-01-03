27588cd83a2743ea34e7d2cfe8bb2aea3fe08e74
ChristopherCaldas <51334879+ChristopherCaldas@users.noreply.github.com>
2023-01-03 14:43:03 -0500

    Update main.js

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ function findSum() {
   var num1 = document.getElementById("num1").value;
   var num2 = document.getElementById("num2").value;
   var num3 = document.getElementById("num3").value;
-  var sum = parseInt(num1) + parseInt(num2) + parseInt(num3);
+  var num4 = document.getElementById("num4").value;
+  var num5 = document.getElementById("num5").value;
+  var num6 = document.getElementById("num6").value;
+  var sum = parseInt(num1) + parseInt(num2) + parseInt(num3) + parseInt(num4) + parseInt(num5) + parseInt(num6);
   document.getElementById("sum").value = sum;
 }
