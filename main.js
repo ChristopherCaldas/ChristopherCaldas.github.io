842ec1c290a0a246938e7791228b79de4e20fb25
ChristopherCaldas <51334879+ChristopherCaldas@users.noreply.github.com>
2023-01-04 09:51:14 -0500

    MMSA Script

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function openMenu2() {
 	document.getElementById("dropdown2").classList.toggle("active");
 }
 
-function findSum() {
+function findSum1() {
   var num1 = document.getElementById("num1").value;
   var num2 = document.getElementById("num2").value;
   var num3 = document.getElementById("num3").value;
@@ -15,5 +15,13 @@ function findSum() {
   var num5 = document.getElementById("num5").value;
   var num6 = document.getElementById("num6").value;
   var sum = parseInt(num1) + parseInt(num2) + parseInt(num3) + parseInt(num4) + parseInt(num5) + parseInt(num6);
+  document.getElementById("sum").value = sum;
+  
+function findSum2() {
+  var num1 = document.getElementById("num7").value;
+  var num2 = document.getElementById("num8").value;
+  var num3 = document.getElementById("num9").value;
+  var num4 = document.getElementById("num10").value;
+  var sum = parseInt(num1) + parseInt(num2) + parseInt(num3) + parseInt(num4);
   document.getElementById("sum").value = sum;
 }
