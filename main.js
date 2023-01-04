f4fe4a4dfd1d6a37720a352de7be2a4a9cb66f90
ChristopherCaldas <51334879+ChristopherCaldas@users.noreply.github.com>
2023-01-04 11:16:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -17,8 +17,14 @@ function findSum() {
   var naturalGasFeed = 32.6 * num1 * 1.1 / 1.055;
   var electricty = 38.6 * num2 
   var water = 8 * num3
+  var catalyst = document.getElementById("num10").value;
   var plantfixedcosts= num4 * num5 / 100 / (900/1000)
   var transport = num6
   
-  document.getElementById("num7").value = naturalGasFeed;
+  document.getElementById("num7").value = Math.round(naturalGasFeed*10)/10;
+  document.getElementById("num8").value = Math.round(electricity*10)/10;
+  document.getElementById("num9").value = Math.round(water*10)/10;
+  document.getElementById("num11").value = Math.round(catalyst*10)/10;
+  document.getElementById("num12").value = Math.round(plantfixedcosts*10)/10;
+  document.getElementById("sum").value = Math.round(transport*10)/10;
 }
