2378afd7a113d9b657674cfb6cab9e7c5273b81a
ChristopherCaldas <51334879+ChristopherCaldas@users.noreply.github.com>
2023-01-04 10:14:27 -0500

    MMSA

--- main.js
+++ main.js
@@ -17,11 +17,3 @@ function findSum1() {
   var sum = parseInt(num1) + parseInt(num2) + parseInt(num3) + parseInt(num4) + parseInt(num5) + parseInt(num6);
   document.getElementById("sum").value = sum;
   
-function findSum2() {
-  var num1 = document.getElementById("num7").value;
-  var num2 = document.getElementById("num8").value;
-  var num3 = document.getElementById("num9").value;
-  var num4 = document.getElementById("num10").value;
-  var sum = parseInt(num1) + parseInt(num2) + parseInt(num3) + parseInt(num4);
-  document.getElementById("sum").value = sum;
-}
